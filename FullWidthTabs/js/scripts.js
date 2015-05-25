b801feb47eb87e2335f9495bb8bafbcc244ec50d
Phillip Creglow <phillipcreglow@msn.com>
2015-05-25 16:14:04 -0400

    Clicky tabs, but index.html refresh is blank until clicking a tab
    @kmattiko

--- FullWidthTabs/js/scripts.js
+++ FullWidthTabs/js/scripts.js
@@ -1,111 +1,37 @@
-// Tracer bullet!
-// it's alive !
-//
-// function rando(limit){
-//   return Math.floor(Math.random() * limit);
-// }
-//
-// function randoImage(){
-//   return BASE_URL + '?' + rando(1000);
-// }
-//
-// $('img').attr('src', randoImage);
-(function(window){
 
-// Variables to use when clicking between tabs
-var tab1 = document.querySelector('.icon-shop');
-var tab2 = document.querySelector('.icon-cup');
-var tab3 = document.querySelector('.icon-food');
-var tab4 = document.querySelector('.icon-lab');
-var tab5 = document.querySelector('.icon-truck');
-var Section1 = document.querySelector('#section-1');
-var Section2 = document.querySelector('#section-2');
-var Section3 = document.querySelector('#section-3');
-var Section4 = document.querySelector('#section-4');
-var Section5 = document.querySelector('#section-5');
+//Collaboration with @kmattiko @JessyRiordan @gatorpazz @sjoyal
 
-// Variables for parentElements used in forEach when removing previous class from tabs/content
-var listItems = document.querySelectorAll('li');
-var contentBlock = document.querySelectorAll('section');
-
-// Adding 'click' event listener to each element; removes active tab and content, adds new active tab and content to appropriate element
-tab1.addEventListener('click', function(){
-  _.forEach(listItems, function(element){
-    if (element.className === 'tab-current') {
-      element.classList.remove('tab-current');
-    };
+  $('a.icon-shop').click(function(){
+   $('section').removeClass('content-current');
+   $('#section-1').addClass('content-current');
+   $('a').removeClass('tab-current');
+   $('a.icon-shop').addClass('tab-current');
   });
-  _.forEach(contentBlock, function(element){
-    if (element.className === 'content-current') {
-      element.classList.remove('content-current');
-    };
-  });
-  this.parentNode.classList.add('tab-current');
-  Section1.classList.add('content-current');
-});
 
-
-tab2.addEventListener('click', function(){
-  _.forEach(listItems, function(element){
-    if (element.className === 'tab-current') {
-      element.classList.remove('tab-current');
-    };
+  $('a.icon-cup').click(function(){
+   $('section').removeClass('content-current');
+   $('#section-2').addClass('content-current');
+   $('a').removeClass('tab-current');
+   $('a.icon-cup').addClass('tab-current');
   });
-  _.forEach(contentBlock, function(element){
-    if (element.className === 'content-current') {
-      element.classList.remove('content-current');
-    };
-  });
-  this.parentNode.classList.add('tab-current');
-  Section2.classList.add('content-current');
-});
 
-
-tab3.addEventListener('click', function(){
-  _.forEach(listItems, function(element){
-    if (element.className === 'tab-current') {
-      element.classList.remove('tab-current');
-    };
+  $('a.icon-food').click(function(){
+   $('section').removeClass('content-current');
+   $('#section-3').addClass('content-current');
+   $('a').removeClass('tab-current');
+   $('a.icon-food').addClass('tab-current');
   });
-  _.forEach(contentBlock, function(element){
-    if (element.className === 'content-current') {
-      element.classList.remove('content-current');
-    };
-  });
-  this.parentNode.classList.add('tab-current');
-  Section3.classList.add('content-current');
-});
 
-
-tab4.addEventListener('click', function(){
-  _.forEach(listItems, function(element){
-    if (element.className === 'tab-current') {
-      element.classList.remove('tab-current');
-    };
+  $('a.icon-lab').click(function(){
+   $('section').removeClass('content-current');
+   $('#section-4').addClass('content-current');
+   $('a').removeClass('tab-current');
+   $('a.icon-lab').addClass('tab-current');
   });
-  _.forEach(contentBlock, function(element){
-    if (element.className === 'content-current') {
-      element.classList.remove('content-current');
-    };
-  });
-  this.parentNode.classList.add('tab-current');
-  Section4.classList.add('content-current');
-});
 
-
-tab5.addEventListener('click', function(){
-  _.forEach(listItems, function(element){
-    if (element.className === 'tab-current') {
-      element.classList.remove('tab-current');
-    };
+  $('a.icon-truck').click(function(){
+   $('section').removeClass('content-current');
+   $('#section-5').addClass('content-current');
+   $('a').removeClass('tab-current');
+   $('a.icon-truck').addClass('tab-current');
   });
-  _.forEach(contentBlock, function(element){
-    if (element.className === 'content-current') {
-      element.classList.remove('content-current');
-    };
-  });
-  this.parentNode.classList.add('tab-current');
-  Section5.classList.add('content-current');
-});
-
-})(window);
